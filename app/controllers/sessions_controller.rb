d877b6e1e7620cbdd3d4505f2cb49ca3ec34ea89
yashihei <yashihei@gmail.com>
2019-04-16 12:37:11 +0900

    Friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # ユーザーログイン後に
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       # エラーメッセージを作成
       flash.now[:danger] = 'invalid email/password combination'
