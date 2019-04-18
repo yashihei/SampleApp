4411eb7e6a20a41426a1f67ef6a79111bad660a2
yashihei <yashihei@gmail.com>
2019-04-18 17:11:45 +0900

    DEPRECATEDなテストが走っていたので修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -45,6 +45,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
-    assert_equal session['forwarding_url'], nil
+    assert_nil session['forwarding_url']
   end
 end
