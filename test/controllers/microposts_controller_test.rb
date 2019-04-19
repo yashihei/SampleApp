24ab0114bc65370361fcf25e5c7239e11e6c6045
yashihei <yashihei@gmail.com>
2019-04-19 17:14:44 +0900

    13.3.5 フィード画面のマイクロポストをテストする

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -18,4 +18,13 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_url
   end
+
+  test "should redirect destroy wrong micropost" do
+    log_in_as(users(:emily))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
 end
