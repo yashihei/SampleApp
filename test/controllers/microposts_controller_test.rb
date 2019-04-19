1d3486cab2dddb28f5535eee51be67f0ff222222
yashihei <yashihei@gmail.com>
2019-04-19 14:15:00 +0900

    Typo

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @micropost = microposts_path(:orange)
+    @micropost = microposts(:orange)
   end
 
   test "should redirect create when not logged in" do
