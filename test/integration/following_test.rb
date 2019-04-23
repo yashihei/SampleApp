4b0df846dabaa5ad8c0a8f479506c0718237ff21
yashihei <yashihei@gmail.com>
2019-04-23 14:36:21 +0900

    Fix testcase

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -27,7 +27,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   test "should follow a user the standard way" do
     assert_difference '@user.following.count', 1 do
-      post relationships_path, xhr: true, params: { followed_id: @other.id }
+      post relationships_path, params: { followed_id: @other.id }
     end
   end
 
